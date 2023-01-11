e82f6c28cc31c27522755a025fb65d176f8bf403
AndrewGithinji <andrewgithinji@hotmail.com>
2023-01-11 12:10:14 +0000

    Using Puppet, install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Installing puppet lint
-package { 'puppet-lint':
-    ensure => '2.1.1',
-    name   => 'puppet-lint',
+# Installing flask
+package { 'flask':
+    ensure => '2.1.0',
+    name   => 'flask',
 }
